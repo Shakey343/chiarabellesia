83716706ae59042114b0cf8dbfb0b3a39943a946
Jake Pople <jakepople@hotmail.co.uk>
2025-12-30 16:27:52 +0000

    navbar created and svgs added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,42 @@
 import { Link } from "react-router";
+import Container from "./Container";
+import greenUnderline from "../assets/green-underline.svg";
+import orangeArrow from "../assets/orange-arrow.svg";
+import redCircle from "../assets/red-circle.svg";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between px-15">
-      <Link to="/" className="text-4xl">
-        Chiara Bellesia
-      </Link>
-      <div className="text-3xl">
-        <Link to="about">about</Link>&nbsp;&nbsp;&nbsp; / &nbsp;&nbsp;
-        <Link to="index">all projects</Link>
+    <Container className="w-full flex justify-between mb-10">
+      <div className="text-4xl">
+        <Link to="/" className="group relative">
+          Chiara Bellesia
+          <img
+            src={greenUnderline}
+            alt=""
+            className="absolute scale-75 opacity-0 transition-all duration-300 ease-out group-hover:opacity-100"
+          />
+        </Link>
       </div>
-    </div>
+      <div className="text-3xl flex justify-between gap-8">
+        <Link to="about" className="group relative">
+          about
+          <img
+            src={orangeArrow}
+            alt=""
+            className="absolute bottom-2 right-20 opacity-0 transition-all duration-300 ease-out group-hover:opacity-100"
+          />
+        </Link>
+        <span>/</span>
+        <Link to="index" className="group relative">
+          all projects
+          <img
+            src={redCircle}
+            alt=""
+            className="absolute -bottom-2 scale-125 opacity-0 transition-all duration-300 ease-out group-hover:opacity-100"
+          />
+        </Link>
+      </div>
+    </Container>
   );
 };
 
