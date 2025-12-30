a17cc9bbf517056299ad0124385439fa6fee8e64
Jake Pople <jakepople@hotmail.co.uk>
2025-12-30 02:10:52 +0000

    setup - tailwind setup - vite - ts - html skeleton created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-col py-10 bg-off-white min-h-screen'>
+        <nav>NAVBAR</nav>
+        <main>MAIN</main>
+        <footer>FOOTER</footer>
+      </div>
+    </>
+  )
+}
+
+export default App
