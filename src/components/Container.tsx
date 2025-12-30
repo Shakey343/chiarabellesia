ef7b1d83ee8a767b73ec501e1580f91101c2a526
Jake Pople <jakepople@hotmail.co.uk>
2025-12-30 16:26:46 +0000

    container component created

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,15 @@
+import type { ReactNode } from "react";
 import cn from "../../utils/cn";
 
-const Container = ({ className, children }) => {
+const Container = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
   return (
-    <div className={cn("max-w-screen mx-6 lg:px-10 h-fit", className)}>
+    <div className={cn("max-w-screen px-4 sm:px-10 h-fit", className)}>
       {children}
     </div>
   );
